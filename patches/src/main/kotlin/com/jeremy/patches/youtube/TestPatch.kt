07f3d8e108d459bf055eca765b653edbf4d9a12b
jch0029987-glitch <jch0029987@gmail.com>
2026-04-08 15:53:03 -0500

    Fix method reference by importing Morph DSL; final TestPatch DSL version

--- patches/src/main/kotlin/com/jeremy/patches/youtube/TestPatch.kt
+++ patches/src/main/kotlin/com/jeremy/patches/youtube/TestPatch.kt
@@ -2,6 +2,7 @@ package com.jeremy.patches.youtube
 
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.patch.bytecodePatch
+import app.morphe.patcher.dsl.method
 
 private const val WATCH_ACTIVITY_DESCRIPTOR = "Lcom/google/android/apps/youtube/app/watchwhile/WatchWhileActivity;"
 
@@ -10,7 +11,7 @@ val initialToastPatch = bytecodePatch(
     description = "Shows a toast on YouTube startup",
     default = true
 ) {
-    // Declare the onCreate method reference inside the patch
+    // Declare the method reference using DSL's method helper
     val onCreate = method(
         WATCH_ACTIVITY_DESCRIPTOR,
         "onCreate",
@@ -19,7 +20,7 @@ val initialToastPatch = bytecodePatch(
 
     execute {
         // Inject instructions at the start of onCreate
-        onCreate.method.addInstructions(
+        onCreate.addInstructions(
             0,
             """
             invoke-static {this, "Jeremy's Custom Patch Loaded!", 1}, Landroid/widget/Toast;->makeText(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;
