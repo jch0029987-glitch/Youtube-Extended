f4adc4b3c43eae2ed1f3bd1373865c3590fa7748
jch0029987-glitch <jch0029987@gmail.com>
2026-04-08 14:58:11 -0500

    fix: use BytecodePatch for Morphe 1.3 sealed class compatibility

--- patches/src/main/kotlin/com/jeremy/patches/youtube/TestPatch.kt
+++ patches/src/main/kotlin/com/jeremy/patches/youtube/TestPatch.kt
@@ -1,19 +1,20 @@
 package com.jeremy.patches.youtube
 
-import app.morphe.patcher.annotation.MorphPatch // Updated import
-import app.morphe.patcher.patch.Patch
-import app.morphe.patcher.context.BytecodeContext // Need this for the type argument
-import app.morphe.patcher.util.hook.MethodHook // New replacement for ProxyMethod
+import app.morphe.patcher.annotation.MorphPatch
+import app.morphe.patcher.patch.BytecodePatch // Use the specialized BytecodePatch
+import app.morphe.patcher.context.BytecodeContext
+import app.morphe.patcher.util.hook.MethodHook
 
-@MorphPatch( // Annotation name updated in v1.3
+@MorphPatch(
     name = "InitialToast",
     description = "Shows a toast message when YouTube is opened.",
     target = "com.google.android.youtube"
 )
-class TestPatch : Patch<BytecodeContext>() { // Added <BytecodeContext> type argument
-    override fun execute(context: BytecodeContext) { // Added context parameter
-        
-        // Using the updated MethodHook API for 2026
+// In v1.3+, we extend BytecodePatch instead of the sealed Patch class
+class TestPatch : BytecodePatch() { 
+
+    override fun execute(context: BytecodeContext) {
+        // Targeted hook for the main YouTube Activity
         MethodHook.builder()
             .className("com.google.android.apps.youtube.app.watchwhile.WatchWhileActivity")
             .methodName("onCreate")
